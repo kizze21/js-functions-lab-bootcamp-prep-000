545ddbed9577e56aacc709c07ebf190a3a53167b
Kizze Harris <kharris668@insite.4cd.edu>
2017-09-14 03:57:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // write your code below!
+var happyHolidays;
+var happysHolidayTo;
+var happyHolidayTo;
+var holidayCountdown;
+
+function happyHolidays(){
+  return "Happy Holidays!"
+}
+
+function happysHolidayTo(name){
+  return `Happy holidays, ${name}!`
+}
+
+function happyHolidaysTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(holiday, number){
+  return `It's ${days} days until ${holiday}!`
+}
