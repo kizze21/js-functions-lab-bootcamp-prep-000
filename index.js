9519cf3a625a32b869ec973f23e9a2927fc60ca7
Kizze Harris <kharris668@insite.4cd.edu>
2017-09-14 04:06:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ function happyHolidays(){
   return "Happy holidays!"
 }
 
-function happysHolidayTo(name){
+function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`
 }
 
-function happyHolidaysTo(holiday, name){
+function happyHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
 
